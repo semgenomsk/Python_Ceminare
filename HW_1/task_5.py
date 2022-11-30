94d06695a3280debabf659ad7e69c6501db1f944
Semen <semgenomsk@yandex.ru>
2022-11-30 16:42:23 +0600

    task5 done

--- HW_1/task_5.py
+++ HW_1/task_5.py
@@ -16,5 +16,6 @@ x1 = int(input("Введите x1 = "))
 y1 = int(input("Введите y1 = "))
 x2 = int(input("Введите x2 = "))
 y2 = int(input("Введите y2 = "))
-dist = (((x2-x1)**2)+((y2-y1)**2))**0.5
-print(dist)
+dist = (((x2 - x1)**2) + ((y2 - y1)**2))**0.5
+# print((int(dist*100))/100)
+print(round(dist,2))
