22f4c17002299677dd8d6cae0889900601f0e475
Semen <semgenomsk@yandex.ru>
2022-12-04 22:38:57 +0600

    task1 done pep8

--- HW_1/task_1.py
+++ HW_1/task_1.py
@@ -16,11 +16,11 @@ True
 False
 """
 
-num=int(input("Введите день недели от 1 до 7: "))
+num = int(input("Введите день недели от 1 до 7: "))
 
 if num < 1 or num > 7:
     print("Номер недели введен некорректно!")
-elif num==6 or num==7:
+elif num == 6 or num == 7:
     print("Выходной! Ура!")
 else:
     print("Вставать пора, собирайтесь на работу!")
