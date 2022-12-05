6b1fac8011d1c2f2658dcfa5a599b07a91dfd754
Semen <semgenomsk@yandex.ru>
2022-12-05 17:52:10 +0600

    task3 done

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,15 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+
+import random
+size = int(input("Введите целое число: "))
+list_num = [round(random.uniform(0, 9), 2) for i in range(size)]
+drob_num = []
+for i in range(0, len(list_num)):
+    n = int(list_num[i] * 100) % 100
+    drob_num.append(n)
+max_drob = max(drob_num)
+min_drob = min(drob_num)
+print(list_num)
+print((max_drob - min_drob) / 100)
