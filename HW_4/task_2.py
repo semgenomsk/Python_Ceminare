678f0e825bdaf2e42163294ce63f430736163416
Semen <semgenomsk@yandex.ru>
2022-12-08 22:49:05 +0600

    task2 done

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,16 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+
+user_set = [int(i) for i in input('Введите последовательность чисел: ').split()]
+sl = {}
+res_list = []
+
+for el in user_set:
+    sl[f'{el}'] = user_set.count(el)
+
+for key, value in sl.items():
+    if value == 1:
+        res_list.append(int(key))
+
+print(res_list)
