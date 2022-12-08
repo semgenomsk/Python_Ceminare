de0fe90990b57123f5fb71f052f3e7775ee575d6
Semen <semgenomsk@yandex.ru>
2022-12-08 22:50:44 +0600

    task4 done

--- HW_4/task_4.py
+++ HW_4/task_4.py
@@ -11,3 +11,21 @@
 3x^2+2x+1=0
 9x^5+7x^4+7x^3+12x^2+8x+18=0
 """
+
+from sympy.abc import x
+from sympy import *
+
+
+def sum_poly(*args):
+    res = []
+    for arg in args:
+        s = arg.replace('x', ' * x').replace('x^', 'x **').replace('=0', '')
+        res.append(collect(s, x))
+    return str(sum(res)).replace('x*', 'x').replace('**', '^')+'=0'
+
+if __name__**'__main__':
+    results=[
+        '5x^5+15x^3+7x+1=0',
+        '10x^5+2x^4+11x^3+9x^2+3x=0'
+    ]
+    print(sum_poly(*results))
