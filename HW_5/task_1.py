5039ff177124868709d7f80f845317f51bcc97f2
Semen <semgenomsk@yandex.ru>
2022-12-18 23:26:35 +0600

    Done task1

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,14 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+
+str_text = "автомобиль букварь вариант пошел снег автобус"
+str_num = []
+
+value = str_text.split()
+for i in value:
+    if "а" not in i and "б" not in i and "в" not in i:
+        str_num.append(i)
+        
+result = " ".join(str_num)
+print(result)
